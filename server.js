4fcd51d1970d587b6c724631cd46ae616ed61f32
Guillaume Perrin <gperrin01@gmail.com>
2015-11-18 11:51:07 +0000

    IP check now on FR instead of UK

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/profiles', cors(), upload.single('picture'), function (req, res) {
       res.send({status: 'error', text: 'An error occured as we are unable to confirm your are in the UK'})
     }
     else if (result.body.country_code !== 'FR') {
-      res.send({status: 'error', text: 'You need to be within the UK to register'})
+      res.send({status: 'error', text: 'You need to be in France to register'})
     }
     else {
       runFaceRecognition();
